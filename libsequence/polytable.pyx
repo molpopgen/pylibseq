a49c16e0061c7733bb2d0a6766ea043a818dd035
molpopgen <krthornt@uci.edu>
2015-10-20 09:35:33 -0700

    docstrings

--- libsequence/polytable.pyx
+++ libsequence/polytable.pyx
@@ -110,6 +110,9 @@ def removeGaps(polyTable p, gapchar = '-'):
     """
     Remove all sites (columns) with gaps.
 
+    :param p: An object derived from :class:`libsequence.polytable.polyTable`
+    :param gapchar: the character representing an alignment gap
+    
     Example:
 
     >>> import libsequence.polytable as pypt
@@ -123,19 +126,79 @@ def removeGaps(polyTable p, gapchar = '-'):
     RemoveGaps(p.thisptr,gc[0])
 
 def isValid(polyTable p):
+    """
+    Return True if p only contains the characters A,C,G,T,N,-,0,1.
+
+    :param p: An object derived from :class:`libsequence.polytable.polyTable`
+    
+    .. note:: This is not case-sensitive
+
+    Example:
+
+    >>> import libsequence.polytable as pypt
+    >>> x = pypt.polySites()
+    >>> x.assign_sep([0.1,0.2,0.3],["ATC","CGA","AT-"])
+    >>> pypt.isValid(x)
+    True
+
+    Now, Z is not a valid character:
+    
+    >>> x.assign_sep([0.1,0.2,0.3],["ATC","CGA","ATZ"])
+    >>> pypt.isValid(x)
+    False
+    """
     return PolyTableValid(p.thisptr)
 
 def removeMono(polyTable p, bint skipOutgroup = False, unsigned outgroup = 0):
+    """
+    Remove invariant sites from p
+
+    :param p: An object derived from :class:`libsequence.polytable.polyTable`
+    :param skipOutgroup: if True, the sequence at position 'outgroup' will not be included in determining if a site is monomorphic
+    :param outgroup: The index of the outgroup sequence in p
+    """
     RemoveInvariantColumns(p.thisptr,skipOutgroup,outgroup)
 
 def freqFilter(polyTable p,unsigned mincount,bint haveOutgroup = False, unsigned outgroup = 0):
+    """
+    Remove all sites with mutation count <= mincount
+
+    :param p: An object derived from :class:`libsequence.polytable.polyTable`
+    :param mincount: Sites with mutation counts <= this value will be removed.  For example, use a value of 2 to remove singletons
+    :param haveOutgroup: if True, the sequence at position 'outgroup' will not be included in determining if a site is monomorphic
+    :param outgroup: The index of the outgroup sequence in p
+    
+    .. note:: If haveOutgroup == True, this is a filter on derived mutation counts, otherwise it is a filter on minor allele counts.  If p is of type :class:`libsequence.polyTable.simData, this is a filter on derived mutation counts.
+    """
     p.thisptr.ApplyFreqFilter(mincount,haveOutgroup,outgroup)
 
 def removeMissing(polyTable p,bint haveOutgroup = False, unsigned outgroup = 0):
+    """
+    Remove all sites missing data (the 'N' or 'n' character)
+
+    :param p: An object derived from :class:`libsequence.polytable.polyTable`
+    :param mincount: Sites with mutation counts <= this value will be removed.  For example, use a value of 2 to remove singletons
+    :param haveOutgroup: if True, the sequence at position 'outgroup' will not be included in determining if a site is monomorphic
+    :param outgroup: The index of the outgroup sequence in p
+    """
     p.thisptr.RemoveMissing(haveOutgroup,outgroup)
 
 def removeMultiHits(polyTable p,bint haveOutgroup = False, unsigned outgroup = 0):
+    """
+    Remove all sites with more than two character states.
+
+    :param p: An object derived from :class:`libsequence.polytable.polyTable`
+    :param haveOutgroup: if True, the sequence at position 'outgroup' will not be included in determining if a site is monomorphic
+    :param outgroup: The index of the outgroup sequence in p
+    """
     p.thisptr.RemoveMultiHits(haveOutgroup,outgroup)
 
 def removeAmbiguous(polyTable p,bint haveOutgroup = False, unsigned outgroup = 0):
+    """
+    Remove all sites with characters not in the set A,G,C,T,N,0,1,-.
+
+    :param p: An object derived from :class:`libsequence.polytable.polyTable`
+    :param haveOutgroup: if True, the sequence at position 'outgroup' will not be included in determining if a site is monomorphic
+    :param outgroup: The index of the outgroup sequence in p
+    """
     p.thisptr.RemoveAmbiguous(haveOutgroup,outgroup)
--- libsequence/summstats.pyx
+++ libsequence/summstats.pyx
@@ -4,6 +4,11 @@ from cpython cimport array
 from cython.operator cimport dereference as deref
 
 cdef class polySNP:
+    """
+    "Factory" class to calculate summary statistics from nucleotide data.
+
+    This is a wrapper around libsequence's Sequence::PolySNP
+    """
     def __cinit__(self,polyTable p,bint haveOutgroup = False, unsigned outgroup = 0, bint totMuts = True):
        self.thisptr = new PolySNP(p.thisptr,haveOutgroup,outgroup,totMuts)
     def thetapi(self):
@@ -45,44 +50,107 @@ cdef class polySNP:
         return self.thisptr.WallsQ()
 
 cdef class polySIM:
+    """
+    "Factory" class to calculate summary statistics from binary data.
+
+    This is a wrapper around libsequence's Sequence::PolySIM
+
+    These objects are constructed from :class:`libsequence.polytable.simData`
+
+    .. note:: 0/1 = ancestral/derived    
+    """
     def __cinit__(self,simData d):
         self.thisptr = new PolySIM(dynamic_cast['SimData*'](d.thisptr))
     def thetapi(self):
+        """
+        "Sum of site heterozygosity."  :math:`\hat\theta_\pi = \sum_i^S\frac{c}{n}\frac{n-c-1}{n-1}`,
+        where :math:`S` is the number of polymorphisms and :math:`n` is the sample size.
+        """
         return self.thisptr.ThetaPi()
     def thetaw (self):
+        """
+        Watterson's estimator of :math:`\theta` from :math:`S`
+        """
         return self.thisptr.ThetaW()
     def thetah (self):
+        """
+        Fay and Wu's estimator of :math:`\theta`.
+        """
         return self.thisptr.ThetaH()
     def thetal (self):
+        """
+        Normalized version of Fay and Wu's estimator of :math:`\theta`.
+        """
         return self.thisptr.ThetaL()
 
     #calculate various numbers related to polymorphism
-    def numpoly(self):                 
+    def numpoly(self):
+        """
+        Number of poylmorphic sites in sample
+        """          
         return self.thisptr.NumPoly() 
     def nummutations (self):
+        """
+        Number of mutations in sample.
+
+        For this class, this is the same as the number of polymorphic sites
+        """          
         return self.thisptr.NumMutations() 
     def numsingletons (self):
+        """
+        Number of singletons (character states 0 or 1)
+        """
         return self.thisptr.NumSingletons() 
     def numexternalmutations (self):
+        """
+        Number of derived mutations (character state 1)
+        """
         return self.thisptr.NumExternalMutations() 
     #summary statistics of the site frequency spectrum
     def tajimasd (self):
+        """
+        Tajima's (1989) D statistics
+        """
         return self.thisptr.TajimasD()
     def hprime (self,bint likeThorntonAndolfatto = False):
+        """
+        Normalized version of Fay and Wu's H statistic
+        """
         return self.thisptr.Hprime(likeThorntonAndolfatto)
     def fulid (self):
+        """
+        Fu and Li's D
+        """
         return self.thisptr.FuLiD()
     def fulif (self):
+        """
+        Fu and Li's F
+        """
         return self.thisptr.FuLiF()
     def fulidstar (self):
+        """
+        Fu and Li's D*
+        """
         return self.thisptr.FuLiDStar()
     def fulifstar (self):
+        """
+        Fu and Li's F*
+        """
         return self.thisptr.FuLiFStar()
     def wallsb(self):
+        """
+        Jeff Wall's B statistic
+        """
         return self.thisptr.WallsB()
     def wallsbprime(self):
+        """
+        Jeff Wall's B' statistic
+        """
         return self.thisptr.WallsBprime()
     def wallsq(self):
+        """
+        Jeff Wall's Q statistic
+        """
         return self.thisptr.WallsQ()
 
 ##functions
