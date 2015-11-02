541f7d72b6c7293796bb41f9caccffc66ca26200
Kevin R. Thornton <krthornt@uci.edu>
2015-11-02 10:14:07 -0800

    setup.py now attempts to make sure that libsequence can be found and used for compiling and linking.  This is redundant with ./configure, but that won't be run if pip is used to install.

---
+++ check_deps/test.cc
@@ -0,0 +1,10 @@
+#include <Sequence/SimData.hpp>
+#include <Sequence/SummStats.hpp>
+
+using namespace std;
+using namespace Sequence;
+
+int main(int argc, char ** argv)
+{
+  auto x = SimData();
+}
