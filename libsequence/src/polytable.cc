686ad5448bacc33411dbee07401f791d0432c97a
molpopgen <krthornt@uci.edu>
2017-04-14 13:22:12 -0700

    got most of polytable redone.

--- libsequence/src/polytable.cc
+++ libsequence/src/polytable.cc
@@ -23,12 +23,49 @@
 #include <Sequence/PolyTable.hpp>
 #include <Sequence/PolySites.hpp>
 #include <Sequence/SimData.hpp>
+#include <Sequence/polySiteVector.hpp>
 
 namespace py = pybind11;
 
 PYBIND11_PLUGIN(polytable)
 {
-    py::module m("polytable","Access to libsequence's polymorphism table classes and related functions");
+    py::module m("polytable", "Access to libsequence's polymorphism table "
+                              "classes and related functions");
+
+    py::class_<Sequence::PolyTable>(m, "PolyTable",
+                                    "Base class for polymorphism tables")
+        .def("GetData", &Sequence::PolyTable::GetData)
+        .def("GetPositions", &Sequence::PolyTable::GetPositions)
+        .def("assign",
+             [](Sequence::PolyTable& p, const Sequence::polySiteVector& v) {
+                 p.assign(v.cbegin(), v.cend());
+             })
+        .def("assign",
+             [](Sequence::PolyTable& p, std::vector<double> pos,
+                std::vector<std::string> d) {
+                 p.assign(std::move(pos), std::move(d));
+             })
+        .def("empty", &Sequence::PolyTable::empty)
+        .def("numsites", &Sequence::PolyTable::numsites)
+        .def("size", &Sequence::PolyTable::size)
+        .def("__len__", [](const Sequence::PolyTable& p) { return p.size(); });
+
+    py::class_<Sequence::PolySites, Sequence::PolyTable>(
+        m, "PolySites", "A polymorphism table for Sequence data.  "
+                        "A/G/C/T/N/0/1 is the alphabet for analysis.")
+        .def("__init__",
+             [](Sequence::PolySites& p, const Sequence::polySiteVector& v) {
+                 new (&p) Sequence::PolySites(v.cbegin(), v.cend());
+             });
+
+    py::class_<Sequence::SimData, Sequence::PolyTable>(
+        m, "SimData",
+        "A polymorphism table for binary data.  0/1 = ancestral/derived.")
+        .def(py::init<std::vector<double>, std::vector<std::string>>())
+        .def("__init__",
+             [](Sequence::SimData& d, const Sequence::polySiteVector& p) {
+                 new (&d) Sequence::SimData(p.cbegin(), p.cend());
+             });
 
     return m.ptr();
 }
