b8ff95f475225e65ee5d5d6bde1cac97a9669b83
molpopgen <krthornt@uci.edu>
2017-04-14 12:52:37 -0700

    update license header

--- libsequence/src/parallel.cc
+++ libsequence/src/parallel.cc
@@ -1,20 +1,20 @@
 //
 // Copyright (C) 2017 Kevin Thornton <krthornt@uci.edu>
 //
-// This file is part of fwdpy11.
+// This file is part of pylibseq.
 //
-// fwdpy11 is free software: you can redistribute it and/or modify
+// pylibseq is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// fwdpy11 is distributed in the hope that it will be useful,
+// pylibseq is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with fwdpy11.  If not, see <http://www.gnu.org/licenses/>.
+// along with pylibseq.  If not, see <http://www.gnu.org/licenses/>.
 //
 #include <pybind11/pybind11.h>
 #include <tbb/task_scheduler_init.h>
