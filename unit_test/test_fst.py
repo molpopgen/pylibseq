802869ef36a95ca11283d5cf30e31e17e2903cdc
molpopgen <krthornt@uci.edu>
2017-04-17 09:09:28 -0700

    cleanup initialization.  gotta move to a fixture, though...

--- unit_test/test_fst.py
+++ unit_test/test_fst.py
@@ -9,26 +9,23 @@ class test_Fst(unittest.TestCase):
             x = [(0.1,"0011"),(0.2,"1100"),
                 (0.3,"0100"),(0.4,"1101"),
                 (0.5,"0101")]
-            d = SimData()
-            d.assign(x)
+            d = SimData(x)
             ##the second argument's sum is > total sample size
-            ##libsequence will throw a SeqException here,
+            ##libsequence will throw a std::runtime_error here,
             ##which gets tranlated to a RuntimeError
             f = Fst(d,[2,3])
     def testShared(self):
         x = [(0.1,"0011"),(0.2,"1100"),
             (0.3,"0100"),(0.4,"1101"),
             (0.5,"0101")]
-        d = SimData()
-        d.assign(x)
+        d = SimData(x)
         f = Fst(d,[2,2])
         self.assertEqual(f.shared(0,1),[0.5])
     def testPriv(self):
         x = [(0.1,"0011"),(0.2,"1100"),
             (0.3,"0100"),(0.4,"1101"),
             (0.5,"0101")]
-        d = SimData()
-        d.assign(x)
+        d = SimData(x)
         f = Fst(d,[2,2])
         p = f.priv(0,1)
         expected = {0:[0.3],1:[0.4]}
@@ -37,8 +34,7 @@ class test_Fst(unittest.TestCase):
         x = [(0.1,"0011"),(0.2,"1100"),
             (0.3,"0100"),(0.4,"1101"),
             (0.5,"0101")]
-        d = SimData()
-        d.assign(x)
+        d = SimData(x)
         f = Fst(d,[2,2])
         p = f.fixed(0,1)
         expected = [0.1,0.2]
@@ -53,8 +49,7 @@ class test_Fst(unittest.TestCase):
             x = [(0.1,"0011"),(0.2,"1100"),
             (0.3,"0100"),(0.4,"1101"),
             (0.5,"0101")]
-            d = SimData()
-            d.assign(x)
+            d = SimData(x)
             f = Fst(d,[2,2])
             #2 is out of range.
             sh = f.shared(2,1)
@@ -63,8 +58,7 @@ class test_Fst(unittest.TestCase):
             x = [(0.1,"0011"),(0.2,"1100"),
             (0.3,"0100"),(0.4,"1101"),
             (0.5,"0101")]
-            d = SimData()
-            d.assign(x)
+            d = SimData(x)
             f = Fst(d,[2,2])
             #2 is out of range.
             sh = f.fixed(2,1)
@@ -73,8 +67,7 @@ class test_Fst(unittest.TestCase):
             x = [(0.1,"0011"),(0.2,"1100"),
             (0.3,"0100"),(0.4,"1101"),
             (0.5,"0101")]
-            d = SimData()
-            d.assign(x)
+            d = SimData(x)
             f = Fst(d,[2,2])
             #2 is out of range.
             sh = f.priv(2,1)
