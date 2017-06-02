59b1fe60015c41dba8785f9048db367c8fba4393
molpopgen <krthornt@uci.edu>
2017-06-02 13:18:43 -0700

    catch exceptions in Fst constructor

--- libsequence/src/fst.cc
+++ libsequence/src/fst.cc
@@ -2,6 +2,7 @@
 #include <pybind11/stl.h>
 #include <Sequence/FST.hpp>
 #include <Sequence/PolyTable.hpp>
+#include <stdexcept>
 
 namespace py = pybind11;
 
@@ -18,8 +19,17 @@ PYBIND11_PLUGIN(fst)
              [](Sequence::FST& fst, const Sequence::PolyTable& pt,
                 const std::vector<unsigned>& config, const bool haveOutgroup,
                 const unsigned outgroup) {
-                 new (&fst) Sequence::FST(&pt, config.size(), &config[0],
-                                          nullptr, haveOutgroup, outgroup);
+                 try
+                     {
+                         new (&fst)
+                             Sequence::FST(&pt, config.size(), &config[0],
+                                           nullptr, haveOutgroup, outgroup);
+                     }
+                 catch (std::runtime_error&)
+                     {
+                         throw std::runtime_error(
+                             "exception caught from Fst constructor");
+                     }
              },
              py::arg("polytable"), py::arg("config"),
              py::arg("haveOutgroup") = false, py::arg("outgroup") = 0)
@@ -28,8 +38,17 @@ PYBIND11_PLUGIN(fst)
                 const std::vector<unsigned>& config,
                 const std::vector<double>& weights, const bool haveOutgroup,
                 const unsigned outgroup) {
-                 new (&fst) Sequence::FST(&pt, config.size(), &config[0],
-                                          &weights[0], haveOutgroup, outgroup);
+                 try
+                     {
+                         new (&fst) Sequence::FST(&pt, config.size(),
+                                                  &config[0], &weights[0],
+                                                  haveOutgroup, outgroup);
+                     }
+                 catch (std::runtime_error&)
+                     {
+                         throw std::runtime_error(
+                             "exception caught from Fst constructor");
+                     }
              },
              py::arg("polytable"), py::arg("config"), py::arg("weights"),
              py::arg("haveOutgroup") = false, py::arg("outgroup") = 0)
@@ -37,13 +56,15 @@ PYBIND11_PLUGIN(fst)
              "Hudson, Slatkin, Maddison (1992) Genetics")
         .def("slatkin", &Sequence::FST::Slatkin, "Slatkin (1991) Genetics")
         .def("hbk", &Sequence::FST::HBK, "Hudson, Boos, Kaplan (1992) MBE")
-        .def("piB", &Sequence::FST::piB, ":math:`\\pi_B= \\frac{\\sum_{i<j}w_i "
-                                         "w_j \\pi_{ij}}{\\sum_{i<j}w_i w_j}`")
+        .def("piB", &Sequence::FST::piB,
+             ":math:`\\pi_B= \\frac{\\sum_{i<j}w_i "
+             "w_j \\pi_{ij}}{\\sum_{i<j}w_i w_j}`")
         .def("piS", &Sequence::FST::piS,
              ":math:`\\pi_S = \\frac{\\sum_i w_i^2 \\pi_{ii}}{\\sum_i w_i^2}`")
-        .def("piT", &Sequence::FST::piT, ":math:`\\pi_T = \\sum_i w_i^2 "
-                                         "\\pi_{ii} + 2\\sum_{i<j}w_i w_j "
-                                         "\\pi_{ij}`")
+        .def("piT", &Sequence::FST::piT,
+             ":math:`\\pi_T = \\sum_i w_i^2 "
+             "\\pi_{ii} + 2\\sum_{i<j}w_i w_j "
+             "\\pi_{ij}`")
         .def("piD", &Sequence::FST::piD,
              ":math:`\\pi_D = \\frac{\\pi_T - \\pi_S}{2 \\sum_{i<j}w_i w_j}`")
         .def("shared", &Sequence::FST::shared)
