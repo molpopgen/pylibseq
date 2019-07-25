f87243f61cdc8bf2707f6a2a77e4daaf7e5a8a93
molpopgen <krthornt@uci.edu>
2019-07-25 13:56:24 -0700

    Add example of getting a VariantMatrix via C++ vectors allocated in another module.

--- tests/test_external_modules.py
+++ tests/test_external_modules.py
@@ -19,6 +19,13 @@ class TestVariantMatrix(unittest.TestCase):
                                        np.array(vm.data)))
         self.assertTrue(np.array_equal(np.array(pos), np.array(vm.positions)))
 
+    def test_create_from_external_cpp_vectors(self):
+        vm = cpptests.create_VariantMatrix_from_cpp_vectors()
+        edata = np.array([0, 1, 1, 0], dtype=np.int8).reshape(2, 2)
+        epos = np.array([0.1, 0.2])
+        self.assertTrue(np.array_equal(vm.data, edata))
+        self.assertTrue(np.array_equal(vm.positions, epos))
+
 
 if __name__ == "__main__":
     unittest.main()
--- tests/variant_matrix.cc
+++ tests/variant_matrix.cc
@@ -1,19 +1,54 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
+#include <pybind11/numpy.h>
 #include <Sequence/VariantMatrix.hpp>
 
 namespace py = pybind11;
+using genotype_vector = std::vector<std::int8_t>;
+using position_vector = std::vector<double>;
 
 py::object
 create_VariantMatrix(py::list g, py::list p)
 {
     auto __l = py::module::import("libsequence");
-    return __l.attr("create_VariantMatrix")(g,p);
+    return __l.attr("create_VariantMatrix")(g, p);
+}
+
+py::object
+create_VariantMatrix_from_cpp_vectors()
+// This is not the only way to go here.
+// You can imagine fancier solutions
+// where the capsules DO NOT delete
+// the pointers.  For example, if the
+// vectors are themselves held in classes
+// and all object lifetimes are managed
+// on the Python side.
+{
+    // We need pointers to our C++ containers
+    genotype_vector* g = new genotype_vector{ 0, 1, 1, 0 };
+    position_vector* p = new position_vector{ 0.1, 0.2 };
+    // These pointers then go into PyCapsule objects,
+    // which are basically smart pointers with custom
+    // deleters
+    auto gc = py::capsule(
+        g, [](void* v) { delete reinterpret_cast<genotype_vector*>(v); });
+    auto pc = py::capsule(
+        p, [](void* v) { delete reinterpret_cast<position_vector*>(v); });
+
+    //These capsules can be the memory buffers of numpy arrays...
+    py::array_t<std::int8_t> ga(g->size(), g->data(), gc);
+    py::array_t<double> pa(p->size(), p->data(), pc);
+    //...from which we may fill libsequence's VariantMatrix
+    //from a new Python module that we've written
+    auto __l = py::module::import("libsequence");
+    return __l.attr("VariantMatrix")(ga, pa);
 }
 
 void
 init_create_VariantMatrix(py::module& m)
 {
     m.def("create_VariantMatrix", &create_VariantMatrix);
+    m.def("create_VariantMatrix_from_cpp_vectors",
+          &create_VariantMatrix_from_cpp_vectors);
 }
 
